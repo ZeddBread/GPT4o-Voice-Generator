15f6b2c0de60c5413c886ff913018c1df921592b
ZeddBread <zedd.mx2a@gmail.com>
2024-11-28 00:44:43 +1100

    All input is working to the OpenAI server
    This should:
    - Generate audio, serverside
    - Give the data to the client
    - Ask data to be written into .wav format
    - Give .wav to client.

--- app/lib/generate-audio.tsx
+++ app/lib/generate-audio.tsx
@@ -7,39 +7,56 @@ import OpenAI from "openai";
 
 const openai = new OpenAI();
 
-export async function generateAudio(text: string, voice: string, fileOutput: string) {
+export async function generateAudio(text: string, voice: string, fileOutput: string, accent: string, emotion: string) {
+  const zAccent = accent.toLowerCase();
+  const zEmotion = emotion.toLowerCase();
+  const zVoice = voice as "alloy" | "echo" | "fable" | "onyx" | "nova" | "shimmer" | "coral" | "verse" | "ballad" | "ash" | "sage";
   try {
     console.log("Generating audio...");
     // Generate an audio response to the given prompt
     const response = await openai.chat.completions.create({
       model: "gpt-4o-audio-preview",
       modalities: ["text", "audio"],
-      audio: { voice: voice as "alloy" | "echo" | "fable" | "onyx" | "nova" | "shimmer" | "coral" | "verse" | "ballad" | "ash" | "sage", format: "wav" },
+      audio: { voice: zVoice, format: "wav" },
       messages: [
         {
-          role: "user",
+          role: "system",
+          content: `You are a beautiful assistant that will speak in a beautiful voice in the specified format: 
+          Accent: ${zAccent}
+          Emotion: ${zEmotion}
+          You will only say the what has been passed to you with your best quality voice.`
+        },
+        {
+          role: "assistant",
           content: text
         }
       ]
     });
 
     // Inspect returned data
-    console.log(response.choices[0]);
-  
-    const audioData = response.choices[0]?.message?.audio?.data;
-    
+    console.log(response.choices);
+
+    const audioData = response.choices[0]?.message?.audio;
+
+    console.log(audioData);
     // Write audio data to a file
     if (audioData) {
-      const audioFile = writeFileSync(
-        fileOutput + ".wav",
-        Buffer.from(audioData, 'base64')
-      );
-      return audioFile;
+      return audioData;
     } else {
       throw new Error("Audio data is undefined");
     }
-    
+
   } catch (error) {
     console.error("Error generating audio:", error);
   }
 }
+
+export async function writeAudioFile(audioData: any, fileOutput: string) {
+  const audioFile = await writeFileSync(
+    fileOutput + ".wav",
+    Buffer.from(audioData.data, 'base64'),
+    { encoding: 'utf-8' }
+  );
+  return audioFile;
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import { generateAudio } from "@/app/lib/generate-audio";
+import { generateAudio, writeAudioFile } from "@/app/lib/generate-audio";
 
 export default function Home() {
   const [text, setText] = useState("");
@@ -11,20 +11,30 @@ export default function Home() {
   const [error, setError] = useState(false);
   const [audioGenerated, setAudioGenerated] = useState(false);
   const [audioFile, setAudioFile] = useState<any>(null);
+  const [audioTranscript, setAudioTranscript] = useState("");
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log({ text, accent, emotion, voice, fileOutput });
     try {
-      const audioFile = await generateAudio(text, voice, fileOutput);
+      const audioData = await generateAudio(text, voice, fileOutput, accent, emotion);
+      const transcript = audioData?.transcript;
+      if (transcript) {
+        setAudioTranscript(transcript);
+      }
+      if (audioData) {
+        const audioFile = await writeAudioFile(audioData, fileOutput);
+        setAudioFile(audioFile);
+      }
       setAudioGenerated(true);
-      setAudioFile(audioFile);
+
     } catch (error) {
       console.error("Error generating audio:", error);
       setError(true);
     }
   };
 
-  
+
 
 
   return (
@@ -33,53 +43,77 @@ export default function Home() {
         onSubmit={handleSubmit}
         className="flex flex-col gap-4 sm:gap-6 items-center w-full max-w-5xl bg-white p-4 sm:p-6 rounded-lg shadow-md"
       >
-        <textarea
-          placeholder="Enter text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none resize-y text-base sm:text-lg md:text-xl"
-        />
-        <input
-          type="text"
-          placeholder="Enter accent"
-          value={accent}
-          onChange={(e) => setAccent(e.target.value)}
-          className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none text-base sm:text-lg md:text-xl"
-        />
-        <input
-          type="text"
-          placeholder="Enter emotion"
-          value={emotion}
-          onChange={(e) => setEmotion(e.target.value)}
-          className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none text-base sm:text-lg md:text-xl"
-          title="Enter emotion"
-        />
-        <select
-          value={voice}
-          onChange={(e) => setVoice(e.target.value)}
-          className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none text-base sm:text-lg md:text-xl"
-          title="Select voice"
-        >
-          <option defaultValue="">Select voice</option>
-          <option value="alloy">Alloy</option>
-          <option value="echo">Echo</option>
-          <option value="fable">Fable</option>
-          <option value="onyx">Onyx</option>
-          <option value="nova">Nova</option>
-          <option value="shimmer">Shimmer</option>
-          <option value="coral">Coral</option>
-          <option value="verse">Verse</option>
-          <option value="ballad">Ballad</option>
-          <option value="ash">Ash</option>
-          <option value="sage">Sage</option>
-        </select>
-        <input
-          type="text"
-          placeholder="Enter file output"
-          value={fileOutput}
-          onChange={(e) => setFileOutput(e.target.value)}
-          className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none text-base sm:text-lg md:text-xl"
-        />
+        <div className="flex flex-row gap-2 w-full justify-center items-center">
+          <label htmlFor="text" className=" w-32 font-bold text-gray-600 sm:text-md md:text-lg">Generated speech:</label>
+          <textarea
+            id="text"
+            placeholder="Enter text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none resize-y text-base sm:text-lg md:text-xl"
+          />
+        </div>
+
+        <div className="flex flex-row col gap-2 w-full justify-center items-center">
+          <label htmlFor="accent" className=" w-32 font-bold text-gray-600 sm:text-md md:text-lg">Accent:</label>
+          <input
+            id="accent"
+            type="text"
+            placeholder="Enter accent"
+            value={accent}
+            onChange={(e) => setAccent(e.target.value)}
+            className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none text-base sm:text-lg md:text-xl"
+          />
+        </div>
+
+        <div className="flex flex-row gap-2 w-full justify-center items-center">
+          <label htmlFor="emotion" className=" w-32 font-bold text-gray-600 sm:text-md md:text-lg">Emotion:</label>
+          <input
+            id="emotion"
+            type="text"
+            placeholder="Enter emotion"
+            value={emotion}
+            onChange={(e) => setEmotion(e.target.value)}
+            className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none text-base sm:text-lg md:text-xl"
+            title="Enter emotion"
+          />
+        </div>
+
+        <div className="flex flex-row gap-2 w-full justify-center items-center">
+          <label htmlFor="voice" className=" w-32 font-bold text-gray-600 sm:text-md md:text-lg">Voice:</label>
+          <select
+            value={voice}
+            onChange={(e) => setVoice(e.target.value)}
+            className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none text-base sm:text-lg md:text-xl"
+            title="Select voice"
+          >
+            <option defaultValue="">Select voice</option>
+            <option value="alloy">Alloy (Less expressive)</option>
+            <option value="echo">Echo (Less expressive)</option>
+            <option value="fable">Fable</option>
+            <option value="onyx">Onyx</option>
+            <option value="nova">Nova</option>
+            <option value="shimmer">Shimmer (Less expressive)</option>
+            <option value="coral">Coral</option>
+            <option value="verse">Verse</option>
+            <option value="ballad">Ballad</option>
+            <option value="ash">Ash</option>
+            <option value="sage">Sage</option>
+          </select>
+        </div>
+
+        <div className="flex flex-row gap-2 w-full justify-center items-center">
+          <label htmlFor="fileOutput" className=" w-32 font-bold text-gray-600 sm:text-md md:text-lg">Filename:</label>
+          <input
+            id="fileOutput"
+            type="text"
+            placeholder="Enter filename"
+            value={fileOutput}
+            onChange={(e) => setFileOutput(e.target.value)}
+            className="w-full p-2 sm:p-3 border border-gray-300 rounded-lg focus:border-primary-color focus:outline-none text-base sm:text-lg md:text-xl"
+          />
+        </div>
+
         <button
           type="submit"
           className="w-full py-2 sm:py-3 bg-primary-color text-white rounded-lg hover:bg-primary-color-dark transition-colors text-base sm:text-lg md:text-xl"
@@ -96,14 +130,16 @@ export default function Home() {
 
         {audioGenerated && (
           <div className="w-full flex flex-col items-center" id="audio-container">
-            <p className="text-base sm:text-lg md:text-xl" >Audio Player</p>
             <div className="w-auto" id="audio-player">
               {!audioFile ? (
                 <div className="w-full flex justify-center items-center">
-                  <div className="loader"></div>
+                  <div className="text-red-500 font-bold">No file yet</div>
                 </div>
               ) : (
-                <audio src={audioFile} controlsList="download" controls autoPlay />
+                <div>
+                  <p className="text-base">{audioTranscript}</p>
+                  <audio src={audioFile} controlsList="download" controls autoPlay />
+                </div>
               )
               }
             </div>
